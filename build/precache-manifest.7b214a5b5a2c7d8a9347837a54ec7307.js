96189246f10fbed3ed8bfd58c07e64370ef6ff40
naresh <telinaresh0@gmail.com>
2021-02-11 05:01:48 +0530

    build

---
+++ build/precache-manifest.7b214a5b5a2c7d8a9347837a54ec7307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797af3f2bdef0647f5a07c9aeec9df48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "807ab40a2b72d10773ef",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ddbbe6028a188ea22746",
+    "url": "/static/js/2.1be97d3b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1be97d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807ab40a2b72d10773ef",
+    "url": "/static/js/main.ce041914.chunk.js"
+  },
+  {
+    "revision": "baaa868e3263093a06d7",
+    "url": "/static/js/runtime-main.ced797e0.js"
+  }
+]);
